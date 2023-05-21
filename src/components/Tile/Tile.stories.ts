b14d0495aa988cd49b0985b8aaa2c241e0409ce0
Gabriel Saito <gabrielpereira050201@hotmail.com>
2023-05-21 17:23:06 -0300

    Chore: Improved comments

--- src/components/Tile/Tile.stories.ts
+++ src/components/Tile/Tile.stories.ts
@@ -16,32 +16,24 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-/**
- * The basic state of the tile
- */
+/** The basic state of the tile */
 export const Default: Story = {}
 
-/**
- * When the tile is selected
- */
+/** When the tile is selected */
 export const Selected: Story = {
 	args: {
 		isSelected: true,
 	},
 }
 
-/**
- * When the tile is the result
- */
+/** When the tile is the result */
 export const Result: Story = {
 	args: {
 		isResult: true,
 	},
 }
 
-/**
- * When the tile is selected and the result
- */
+/** When the tile is selected and the result */
 export const SelectedAndResult: Story = {
 	args: {
 		isSelected: true,
